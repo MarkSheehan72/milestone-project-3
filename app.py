4a8af267efecf25b909aa3a5ccb22c3b586d343a
Mark Sheehan <marksheehan72@hotmail.com>
2019-06-24 17:51:26 +0000

    Index page functionality done, addrecipe.html functional, recipes.html up but in need of a lot of work, other pages which may be deleted later.

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
 import os
-from flask import Flask 
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo 
+from bson.objectid import ObjectId 
 
 app = Flask(__name__) 
 
-@app.route('/') 
-def hello(): 
-    return 'Hello world ...again' 
+app.config["MONGO_DBNAME"] = 'cook_book'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-chhcw.mongodb.net/cook_book?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
     
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("addrecipe.html", recipes=mongo.db.recipes.find(), categories=mongo.db.categories.find())
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('index'))
+
+@app.route('/get_recipes') 
+def get_recipes():  
+    return render_template("recipes.html", recipes=mongo.db.recipes.find(), categories=mongo.db.categories.find())
+
+    
+@app.route('/cuisine', methods=['POST'])
+def cuisine():
+    return render_template("cuisine.html", recipes=mongo.db.recipes.find(), cuisine=mongo.db.recipes.cuisine.find(), categories=mongo.db.categories.find())
+
+@app.route('/cuisine_recipes/<cuisine_choice>', methods=['POST'])
+def cuisine_recipes(cuisine_choice):
+    the_cuisine = mongo.db.recipes.find({'cuisine': ObjectId(cuisine_choice)})
+    return render_template("cuisine_recipes.html", cuisine=the_cuisine, recipes=mongo.db.recipes.find(), categories=mongo.db.categories.find())
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), 
