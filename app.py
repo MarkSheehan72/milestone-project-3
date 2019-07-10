15c88085931144a51712506b14bdd0b97a0dd527
Ubuntu <ubuntu@ip-172-31-47-150.ec2.internal>
2019-07-10 20:28:28 +0000

    Added fonts

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    recipes=mongo.db.recipes.find()
+    return render_template("index.html", recipes=recipes)
 
 # Add recipe page
 @app.route('/add_recipe')
